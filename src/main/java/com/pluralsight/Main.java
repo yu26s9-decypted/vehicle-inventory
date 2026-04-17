31bcdaf1946c62db5e7762f7fba0985cd43700de
andy <andyt.cyb@gmail.com>
2026-04-17 00:26:52 -0400

    Fixed displayVehicle method

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,8 @@
 package com.pluralsight;
 
 import java.io.Console;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.*;
 
 
 public class Main {
@@ -13,20 +11,29 @@ public class Main {
 
     static void main() {
 
-        Vehicle fordExplorer = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
-        Vehicle toyotaCamary = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
-        Vehicle chevroletMalibu = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
-        Vehicle hondaCivic = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
-        Vehicle subaruOutback = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
-        Vehicle jeepWrangler = new Vehicle(101126, "Jeep Wrangler", "Yellow", 35000, 16500);
+//        Vehicle fordExplorer = new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500);
+//        Vehicle toyotaCamary = new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000);
+//        Vehicle chevroletMalibu = new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700);
+//        Vehicle hondaCivic = new Vehicle(101124, "Honda Civic", "White", 70000, 7500);
+//        Vehicle subaruOutback = new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500);
+//        Vehicle jeepWrangler = new Vehicle(101126, "Jeep Wrangler", "Yellow", 35000, 16500);
+
+
+
+        inventory.add(new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500));
+        inventory.add(new Vehicle(101122, "Toyota Camry", "Blue", 60000, 11000));
+        inventory.add(new Vehicle(101123, "Chevrolet Malibu", "Black", 50000, 9700));
+        inventory.add(new Vehicle(101124, "Honda Civic", "White", 70000, 7500));
+        inventory.add(new Vehicle(101125, "Subaru Outback", "Green", 55000, 14500));
+        inventory.add(new Vehicle(101126, "Jeep Wrangler", "Yellow", 35000, 16500));
 
         int userBuyer = vehicleSystem();
 
-        inventory.add(new Vehicle(101121, "Ford Explorer", "Red", 45000, 13500));
         switch (userBuyer) {
             case 1: // List all vehicles
                 System.out.println("Selected 1");
                 listAllVehicles(inventory, inventory.size() );
+                break;
             case 2:
                 findVehiclesByPrice();
             case 3:
@@ -38,7 +45,7 @@ public class Main {
             case 6: // Exit
                 System.out.println("Goodbye!");
             default:
-                System.out.println("Wrong");
+                vehicleSystem();
         }
     }
 
@@ -48,20 +55,23 @@ public class Main {
 
     private static void displayVehicles(Vehicle[] vehicles, int numberOfVehicles) {
 
-        System.out.println("Vehicle ID   Make / Model        Color       Mileage        Price");
+        System.out.printf("Vehicle ID     Make / Model       Color     Mileage        Price \n");
         for (int i = 0; i < numberOfVehicles; i++) {
             Vehicle v = vehicles[i];
-            System.out.printf("%10d %-20s %-10s %10d %14.2f\n",
+            NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+            System.out.printf("%10d %20s %10s %,10d %15s\n",
                     v.getVehicleId(),
                     v.getMakeModel(),
                     v.getColor(),
                     v.getOdometer(),
-                    v.getPrice());
+                    usFormat.format(v.getPrice())
+            );
         }
     }
 
 
     private static void searchByColor() {
+
     }
 
     private static void searchByPriceRange() {
